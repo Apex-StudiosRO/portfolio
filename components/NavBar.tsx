f3000f69bb8b0c0e39b03ee2683bfe62bdce49c4
iampandaro <matei.florentin.cucu@cneminescu.ro>
2025-11-24 21:45:40 +0200

    Made changes to Navbar ( Buttons work )

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,32 +5,25 @@ import Link from "next/link";
 import Button from "./Button";
 
 const NavBar = () => {
-    const scrollToSection = (sectionId: string) => {
-        const element = document.getElementById(sectionId);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
     return (
         <nav className="fixed p-2 top-1 w-screen flex items-center justify-center z-50">
             <div className="flex py-2 px-6 bg-blue-300/15 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm border-2 border-blue-300/80 gap-6 items-center">
-                <button onClick={() => scrollToSection('main')} className="cursor-pointer">
+                <Link href="/">
                     <Image src="/logo.png" alt="Logo" width={30} height={30} className="rounded-full" />
-                </button>
+                </Link>
                 <div className="flex gap-4 items-center justify-center">
-                    <button onClick={() => scrollToSection('main')}>
+                    <Link href="/">
                         <Button variant="underline">Home</Button>
-                    </button>
-                    <button onClick={() => scrollToSection('about')}>
+                    </Link>
+                   <Link href="/about">
                         <Button variant="underline">About</Button>
-                    </button>
-                    <button onClick={() => scrollToSection('projects')}>
+                    </Link>
+                    <Link href="/projects">
                         <Button variant="underline">Projects</Button>
-                    </button>
-                    <button onClick={() => scrollToSection('contact')}>
+                    </Link>
+                    <Link href="/contact">
                         <Button variant="underline">Contact</Button>
-                    </button>
+                    </Link>
                 </div>
             </div>
         </nav>
